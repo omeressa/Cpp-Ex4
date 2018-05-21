083b4be1f5f314994f140f830cb8805f37938082
OmarEssa <33825687+omeressa@users.noreply.github.com>
2018-05-21 12:11:56 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -20,28 +20,28 @@ public:
              /*-----Add-----*/
 	CircularInt operator+( int);
 	CircularInt operator+(const CircularInt&);
-	CircularInt& operator+=(int);
-	CircularInt&  operator++();
-	CircularInt  operator++(int);
+	CircularInt& operator+=(int);/////////////////////////////////////////
+	CircularInt&  operator++();//////////////////////////////////////////
+	CircularInt  operator++(int);///////////////////////////////////////
 
 	     /*-----Sub-----*/
-  friend CircularInt operator-(const CircularInt& );
-	friend CircularInt operator-( const int, const CircularInt&);
+  friend CircularInt operator-(const CircularInt& );//////////////////////////////   -CirculInt (hour)
+	friend CircularInt operator-( const int, const CircularInt&);//////////////////////  num - CirculInt
 	CircularInt operator--();
 	CircularInt& operator-=(int);
 
              /*-----Mult-----*/
-	CircularInt &operator*=(const int);
+	CircularInt &operator*=(const int);/////////////////////////////////  CirculInt *num
 	friend CircularInt operator * (int, CircularInt const &);
 	
 	     /*-----Div-----*/
-	friend CircularInt operator/(const CircularInt&, const int );
+	friend CircularInt operator/(const CircularInt&, const int );//////////////////////////////// circulInt / num
 	friend CircularInt operator / (CircularInt const &, CircularInt const &);
 	friend CircularInt operator / (int , CircularInt const &);
         CircularInt operator/=(const int);
 		
              /*-----Stream-----*/
-	friend ostream& operator<<(ostream&, const CircularInt);
+	friend ostream& operator<<(ostream&, const CircularInt);////////////////// operator copy
 	//friend istream& operator >> (istream & ,CircularInt &);
 	
 	     /*-----Comp-----*/
